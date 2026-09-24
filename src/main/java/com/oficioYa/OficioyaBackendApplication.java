1408ae78292ee4b0826959038018f079483f4189
Jose Martinez <115952610+JoseMartinez883@users.noreply.github.com>
2026-09-24 02:51:31 -0500

    fix: add directories structure

--- src/main/java/com/oficioYa/OficioyaBackendApplication.java
+++ src/main/java/com/oficioYa/OficioyaBackendApplication.java
@@ -1,4 +1,4 @@
-package com.oficioya;
+package com.oficioYa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/oficioYa/OficioyaBackendApplicationTests.java
+++ src/test/java/com/oficioYa/OficioyaBackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.oficioya;
+package com.oficioYa;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
